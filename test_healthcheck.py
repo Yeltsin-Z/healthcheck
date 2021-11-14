50ae2e1791d2e973ceefc0d8f8cede1484245c06
Yeltsin <93512039+Yeltsin-Z@users.noreply.github.com>
2021-11-14 21:21:50 +0530

    Update test_healthcheck.py

--- test_healthcheck.py
+++ test_healthcheck.py
@@ -1,10 +1,7 @@
-import os
 import requests
 import commons as cmn
 
 def test_dtml():
-    # my_list = os.listdir('./')
-    # folder_names = cmn.valid_folders(my_list)
     f_names = ['102', '108']
     for i in range(len(f_names)):
         url = "https://cube-staging.drivetrain.ai/cube/check"
